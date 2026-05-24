cece47f4bf97d4c8ac58e987641442ffddc1d4c5
Tobias Domaas <48107127+Bissas@users.noreply.github.com>
2026-05-24 17:46:04 +0200

    Update main.ts

--- main.ts
+++ main.ts
@@ -19,6 +19,7 @@ namespace deploii{
     }
 
     //% blockId=deploii-interval
+    //% block="every $t ms, do $f"
     //% t.min=1 t.defl=1000
     //% f.shadow=functions
     export function interval(t: number, f: () => void) : void {
