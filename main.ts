2a3f5a58becc0a95e74045c9a720f50e5a26c4ab
Tobias Domaas <48107127+Bissas@users.noreply.github.com>
2026-05-24 16:39:19 +0200

    initial commit

--- main.ts
+++ main.ts
@@ -1,1 +1,16 @@
+/**
+ * deploii for micro:bit
+ */
+//% weight=100 color=#ffffff icon="\uf1ec"
 
+namespace deploii{
+
+    function connect(ssid: string, password: string) : void {
+
+    }
+
+    function send(dataID: string, data: any) : void {
+        
+    }
+
+}
