e7677d90f43969100ed36f821fd5a03f550a7162
Tobias Domaas <48107127+Bissas@users.noreply.github.com>
2026-05-24 17:00:28 +0200

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,16 +1,27 @@
-/**
- * deploii for micro:bit
- */
-//% weight=100 color=#ffffff icon="\uf1ec"
-
 namespace deploii{
 
-    export function connect(ssid: string, password: string) : void {
-
+    //% blockId=deploii-loop
+    //% block="deploii loop"
+    export function loop():void {
+        return;
     }
 
-    export function send(dataID: string, data: any) : void {
-        
+    //% blockId=deploii-connect
+    //% block="connect to $ssid, with password $password"
+    export function connect(ssid: string, password: string) : void {
+        return;
+    }
+
+    //% blockId=deploii-send
+    //% block="send $data to $datastream" 
+    export function send(datastream: string, data: any) : void {
+        return;
+    }
+
+    //% blockid=deploii-interval
+    //% block="do $f every $t ms"
+    export function interval(f:()=>void, t:number) : void {
+        return;
     }
 
 }
